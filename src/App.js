fa8783663b089b3c5083be21a04c156d0330abe2
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-30 03:29:41 +0200

    Feat: import escape-string-regexp, sort-by
    after intall them npm install --save escape-string-regexp sort-by

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 import logo from './logo.svg';
 import './App.css';
 import MapContainer from './components/MapContainer'
