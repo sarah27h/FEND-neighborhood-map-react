dc8bbda07939cff422cdb95aedf147e623fbbe75
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-01 03:17:21 +0200

    Feat: add <Footer/> component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+    render() {
+  
+      return (
+          <div className="footer">
+              <p>Copyright © Museums Guide - data sources/APIs used Foursquare's API</p>
+          </div>
+      )
+    }
+}
+
+
+export default Footer;
