cb4972f8aede514def22ac04431cca77d0da9a4a
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-02 02:23:48 +0200

    Feat: add hamburger menu and style it

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,14 @@ export class Header extends Component {
     render() {
   
       return (
-          <header className="header">
-              <h1>Museums Guide</h1>
-          </header>
+        <header className="header">
+            <a id="menu" className="header__menu">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
+                </svg>
+            </a>
+            <h1>Museums Guide</h1>
+        </header>
       )
     }
 }
