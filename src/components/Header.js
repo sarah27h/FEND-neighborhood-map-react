1a034251978d4050e4db0fddf5631c2cf9564a4a
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-03 21:57:50 +0200

    Style: remove inline style, change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,11 @@ export class Header extends Component {
   
       return (
         <header className="header">
-            <a id="menu" className="header__menu">
+            <a id="menu" className="header_menu">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
                 </svg>
             </a>
-            <h1>Museums Guide</h1>
         </header>
       )
     }
--- src/components/LocationFilter.js
+++ src/components/LocationFilter.js
@@ -5,12 +5,10 @@ export class LocationFilter extends Component {
     
     render() {
   
-      console.log(this.props.locations);
-  
       return (
           <div className="location_filter">
 
-            <h2>Location Filter</h2>
+            <h3>Museums Guide</h3>
                 <input 
                     type="text" 
                     placeholder="Search by museum name"
