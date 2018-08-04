47a6b272d554ad387ed86d03c3f60ccfe0bf3281
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-04 19:28:52 +0200

    Feat: change hamburg icon to button
    change hamburg icon to button to
    be semantic and clickable by enter for keyboard users
    also add aria-label="menu" for it to indicate its purpose.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ function Header (props) {
   
     return (
         <header className="header">
-            <a id="menu" className="header_menu" onClick={props.handleHamburgerClick}>
+            <button aria-label="menu" id="menu" className="header_menu" onClick={props.handleHamburgerClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
                 </svg>
-            </a>
+            </button>
         </header>
     )
 }
