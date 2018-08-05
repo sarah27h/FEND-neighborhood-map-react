bd9c79153c37d8f63c9670556fdcc9840bce11a8
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-06 00:15:06 +0200

    Fix: remove redundant role for <header>
    as it is a semantic element

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header (props) {
   
     return (
-        <header role="banner" className="header">
+        <header className="header">
             <button aria-label="menu" id="menu" className="header_menu" onClick={props.handleHamburgerClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"></path>
