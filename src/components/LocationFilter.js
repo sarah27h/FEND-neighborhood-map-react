afbcfb9a53596247a96a8ca8d0e9d08ece06b4a7
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-08-10 01:42:16 +0200

    Refactor: A11y add ARIA label for this searchField

--- src/components/LocationFilter.js
+++ src/components/LocationFilter.js
@@ -6,11 +6,12 @@ function LocationFilter (props) {
         <div className="location_filter">
 
         <h3>Museums Guide</h3>
-        <label hidden htmlFor="search" aria-hidden="false">Search by museum name</label>
+        {/* <label hidden htmlFor="search" aria-hidden="false">Search by museum name</label> */}
             <input 
                 id="search"
-                type="text" 
-                placeholder="Search by museum name"
+                type="text"
+                aria-label="Search by museum name"
+                placeholder="Start typing name"
                 className="search_field"
                 value={props.query}
                 onChange={(event) => {props.onUpdateQuery(event.target.value)}} />
